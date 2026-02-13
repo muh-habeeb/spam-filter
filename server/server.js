6dfe1dab15c1fedb4c7ebc710b87f1fdde63247d
Muhammed Habeeb Rahman KT <muhammadhabeebrahmam@gmail.com>
2026-02-14 00:27:32 +0530

    croos fixed iguss

--- server/server.js
+++ server/server.js
@@ -1,32 +1,43 @@
-import express, { urlencoded } from 'express';
+import express, { urlencoded } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import predict from "./controller/predict.controller.js";
-import axios from 'axios';
+import axios from "axios";
 dotenv.config();
-
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "localhost:8000",
+      "http://localhost:8000",
+      "localhost:5000",
+      "http://localhost:5000",
+      "https://spam-filter-4tjo.onrender.com",
+      "http://spam-filter-4tjo.onrender.com",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
 app.use(express.json(urlencoded({ extended: true })));
 
 app.use("/api/predict", predict);
 
-app.get("/",async (req, res) => {
-    const response = await axios.get("http://localhost:8000/")
-    res.status(200).json({ status: "OK",re:response.data });
+app.get("/", async (req, res) => {
+  const response = await axios.get("http://localhost:8000/");
+  res.status(200).json({ status: "OK", re: response.data });
 });
 
 const ML_API_URL = process.env.ML_API_URL;
 if (!ML_API_URL) {
-    console.error("ML_API_URL is not defined in environment variables");
-    process.exit(1);
-
+  console.error("ML_API_URL is not defined in environment variables");
+  process.exit(1);
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
